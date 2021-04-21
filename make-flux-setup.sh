5e82cd847d9e9958097c30cc953a231bc3be3270
Rob Zimmelman <rob.zimmelman@themill.com>
2021-04-21 11:05:16 -0400

    test

--- make-flux-setup.sh
+++ make-flux-setup.sh
@@ -1,3 +1,2 @@
-#
 set GHUSER = "robzim"
 fluxctl install --git-user=${GHUSER} --git-email=${GHUSER}@users.noreply.github.com --git-url=git@github.com:${GHUSER}/docker-dev --git-path=kubernetes/secrets,kubernetes/configmaps,kubernetes/deployments --git-branch=flux-test --namespace=flux | k apply -f - 
